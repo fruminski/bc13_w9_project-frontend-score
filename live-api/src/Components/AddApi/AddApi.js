36a42305687bf631e166ec484323d8039358c16c
yahyam13 <yahyamohamud13@hotmail.com>
2022-11-22 12:32:47 +0000

    input and button

---
+++ live-api/src/Components/AddApi/AddApi.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function AddApi() {
+return(
+<div>
+ <input
+ type="text"
+className = "input"
+></input>
+<button type="button">Add An API</button>
+</div>
+)}
+
+export default AddApi
--- live-api/src/Components/App/App.js
+++ live-api/src/Components/App/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import Header from "../Header/Header";
 import ListOfApis from "../ListOfApis/ListOfApis";
 import "./App.css";
+import AddApi from "../AddApi/AddApi";
 
 function App() {
   return (
     <div className="app-container">
       <Header />
+      <AddApi />
       <ListOfApis />
     </div>
   );
